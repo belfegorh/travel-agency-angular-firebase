8720a8ec16f711232bc7f8d52413a34d4fe7ce82
Francisco Hurtado <belfegorh@gmail.com>
2023-09-02 17:12:48 -0500

    login messages

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { LoginService } from './../../services/login.service';
 import { Component } from '@angular/core';
 import { Validators, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,7 +12,11 @@ export class LoginComponent {
   loginForm: FormGroup;
 
   submitted = false;
-  constructor(private loginService: LoginService, public router: Router) {}
+  constructor(
+    private loginService: LoginService,
+    public router: Router,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit() {
     this.loginForm = new FormGroup({
@@ -22,6 +27,22 @@ export class LoginComponent {
 
   async onSubmit() {
     this.submitted = true;
-    const res = await this.loginService.firebaseLogin(this.loginForm.value);
+    const res = await this.loginService
+      .firebaseLogin(this.loginForm.value)
+      .catch((err) => err);
+    if (res?.message === 'Firebase: Error (auth/invalid-email).') {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Usuario/Password incorrectos',
+      });
+    } else {
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Ingresando...',
+      });
+    }
+    this.submitted = false;
   }
 }
